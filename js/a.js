871ccadde44ed69c76faf8c8fa427fd1a23f1c59
ryf123 <ryf123@gmail.com>
2013-09-29 22:31:17 -0700

    Delete unnecessary files

--- js/a.js
+++ js/a.js
@@ -1,26 +1,12 @@
-var timeinterval=3000;
-var count=-1;
-var var_interval = setInterval(function(){change_pic()},timeinterval);
 $(document).ready(function () {
-	var index;
-	$('.small-image').hover(function() {
-		index = $('.small-image').index($(this)); 				
-    	$('.display-image').eq(index).removeClass("opClass");
-    	clearInterval(var_interval);		
+	$('.browse').hover(function(){
+		$(this).addClass('browse-hover');
+		$(this).find("ul").show();
 	},
 	function(){
-		$('.display-image').eq(index).addClass("opClass");
-		var_interval = setInterval(function(){change_pic()},timeinterval);
+		$(this).removeClass('browse-hover');
+		$(this).find("ul").hide();
 	}
-);
+	);
 }
 );
-function change_pic(){
-	if(count==-1){
-		$('.display-image').eq(2).addClass("opClass");
-	}
-	$('.display-image').eq(count).addClass("opClass");	
-	count++;
-	$('.display-image').eq(count).removeClass("opClass");	
-	if(count==2){count=-1;}		
-}
