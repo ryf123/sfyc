3780901fc172540c327b20be677611cfe20f4156
chengshi007 <chengshi517@hotmail.com>
2013-10-24 02:21:03 +0800

    Important commit 10-24
    Merge Fu's modification of myspace.html into myspace.php with new css
    and js files, clean up folders and update backup folder. Now, the
    picture post and article post are clickable and good to test the basic
    functions.

--- BackUp/adoption.php
+++ BackUp/adoption.php
@@ -1,9 +1,18 @@
+<?php  
+	session_start();
+	if(isset($_SESSION['LOGIN'])){
+		$sign_in=TRUE;	
+	}
+	else{
+		$sign_in=FALSE;
+	}
+?>
 <!DOCTYPE html>
-
+<html>
 	<head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-		<title>i宠网-我的最i</title>
+		<title>规范领养平台-i宠</title>
 		<meta name="description" content="" />
 		<meta name="author" content="Shi" />
 		<meta name="viewport" content="width=device-width; initial-scale=1.0" />
@@ -11,7 +20,7 @@
 		<link rel="shortcut icon" href="/favicon.ico" />
 		<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 
-   	    <link rel="stylesheet" type="text/css" href="css/style-mainpage.css"/>
+   	    <link rel="stylesheet" type="text/css" href="css/style-adoption.css">
 	</head>
 
 	<body>
@@ -21,8 +30,21 @@
 				<!-- 修改内容：这里添加了登陆按钮的预置位-->
 				<div id="logo1" class="leaderelements">OurSite</div>
 				<div id="logo2" class="leaderelements">Tencent</div>
-				<div id="logo3" class="leaderelements">Sign In</div>
-				<div id="logo4" class="leaderelements">Register</div>
+				<div id="logo3" class="leaderelements">
+				<?php
+					if($sign_in)
+						echo "<a href='logout.php'>Sign Out</a>";
+					else 
+						echo "<a href='login.php'>Sign In</a>";
+				?>
+				</div>
+				<?php
+					if(!$sign_in){
+					echo "<div id='logo4' class='leaderelements'>";
+					echo "<a href='Register.php'>Register</a>";
+					echo "</div>";
+					}
+				?>
 				<br/>
 				<hr/>
 				<br/>
@@ -41,22 +63,22 @@
 				</div>
 				<div id="menuinterface">
 					<div id="divoption0" class="divoptions">
-						<a href="index.html">首页</a>
+						<a href="index.php">首页</a>
 					</div>
 					<div id="divoption1" class="divoptions">
-						<a href="adoption.html">宝贝领养</a>
+						<a href="adoption.php">宝贝领养</a>
 					</div>
 					<div id="divoption2" class="divoptions">
-						<a href="news.html">新鲜事</a>
+						<a href="news.php">新鲜事</a>
 					</div>
 					<div id="divoption3" class="divoptions">
-						<a href="article.html">长文干货</a>
+						<a href="article.php">长文干货</a>
 					</div>
 					<div id="divoption4" class="divoptions">
-						<a href="apps.html">站内应用</a>
+						<a href="apps.php">站内应用</a>
 					</div>
 					<div id="divoption5" class="divoptions">
-						<a href="myspace.html">个人主页</a>
+						<a href="myspace.php">个人主页</a>
 					</div>
 					<input class="search-input2" placeholder="Search">
 				</div>
---
+++ BackUp/constants.php
@@ -0,0 +1,7 @@
+<?php
+	define("DB_SERVER","68.178.142.155 ");
+	define("DB_USER","SFYC");
+	define("DB_PWD","Firewall12!");
+	define("DB_NAME","SFYC");
+	define("USER_TABLE","users");
+?>
